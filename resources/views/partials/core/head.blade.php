f6d776b2ef2d4c6667a58228bc46bf7a21d6a432
Nathan Langer <nathan.langer@nullstack.co.uk>
2018-05-23 12:15:02 +0100

    menu

--- resources/views/partials/core/head.blade.php
+++ resources/views/partials/core/head.blade.php
@@ -26,10 +26,10 @@
     <link href='/Resources/LESS' rel='stylesheet'> 
 
     <!-- FontAwesome -->
-    <link rel='stylesheet' href='/css/fontawesome.min.css' integrity='sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp' crossorigin='anonymous'>
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.0.13/css/all.css' integrity='sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp' crossorigin='anonymous'>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Mobile Nav -->
-  <link rel="stylesheet" href="/css/mobilemenu.min.css">
+  <link rel="stylesheet" href="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.css">
 </head>
